112c9f4f1eeeed162253a91fc04c01022588beb8
nichraff <24k10010@student.unika.ac.id>
2025-12-03 22:06:47 +0700

    update login register

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', [AuthController::class, 'index'])->name('login');
 
 Route::resource('products', ProductController::class);
+
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+Route::get('/admin/dashboard', [AuthController::class, 'dashboard'])->name('admin.dashboard');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/reset-password', [AuthController::class, 'resetPassword'])->name('reset.password');
+Route::post('/reset-password', [AuthController::class, 'updatePassword'])->name('reset.password.post');
+Route::get('/login', [AuthController::class, 'index'])->name('login');
